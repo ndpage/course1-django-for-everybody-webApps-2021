a1987f49a31e41c54ac36a521693388b8ea3e2ae
Page Nathan (DC-MH/ENG1.13-NA) <Nathan.Page@boschrexroth-us.com>
2021-06-28 15:32:13 -0400

    Code for week 2 on PythonAnywhere.com

---
+++ week2/pythonanywhere.py
@@ -0,0 +1,10 @@
+# mysite on PythonAnywhere
+# Basic URLs file in polls directory
+
+# Created web applicationi called Polls by following the tutorial on https://www.dj4e.com
+
+# !!!!!!  Week 2 code is on PythonAnywhere.com  !!!!!
+#   username: ndpage
+#   password: py*******21
+
+
